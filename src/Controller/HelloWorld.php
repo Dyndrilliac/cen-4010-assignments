10569ff7d928d53fd30e74278a8350a75a198f58
Matthew Boyette <Dyndrilliac@gmail.com>
2016-09-16 15:41:15 -0400

    Update HelloWorld.php

--- src/Controller/HelloWorld.php
+++ src/Controller/HelloWorld.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @file
- * Contains \Drupal\restaurant_manager\Controller\HelloController.
+ * Contains \Drupal\restaurant_manager\Controller\HelloWorld.
  */
 
 namespace Drupal\restaurant_manager\Controller;
