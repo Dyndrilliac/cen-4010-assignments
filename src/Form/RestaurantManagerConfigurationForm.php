78b8448936f3eb62c09f643fb5bd2ab399ebd617
Matthew Boyette <Dyndrilliac@gmail.com>
2016-11-04 15:15:30 -0400

    Update RestaurantManagerConfigurationForm.php

--- src/Form/RestaurantManagerConfigurationForm.php
+++ src/Form/RestaurantManagerConfigurationForm.php
@@ -22,9 +22,7 @@ class RestaurantManagerConfigurationForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    //$default_config = \Drupal::config('resaurant_manager.settings');
-    //$config = $this->getConfiguration();
-    
+
     $form['restaurant_name'] = array (
       '#type' => 'textfield',
       '#title' => $this->t('Restaurant Name'),
@@ -56,6 +54,5 @@ class RestaurantManagerConfigurationForm extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     // Handle submitted form data.
-    //$this->setConfigurationValue('restaurant_name', $form_state->getValue('restaurant_name'));
   }
 }
