b28ac4f973e1f39fdebbf9fd3872210d8d43a545
Matthew Boyette <Dyndrilliac@gmail.com>
2016-11-06 23:06:14 -0500

    Update and rename VMenuStatusController.php to StatusController.php

--- src/Controller/StatusController.php
+++ src/Controller/StatusController.php
@@ -3,17 +3,15 @@
  * @file
  * Contains \Drupal\vmenu\Controller\StatusController.
  */
-
-/**Controller is name of file*/
 namespace Drupal\vmenu\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
-class VMenuStatusController extends ControllerBase {
+class StatusController extends ControllerBase {
   public function content() {
     return array(
         '#type' => 'markup',
-        '#markup' => $this->t('A real-time status update will be displayed here in later iterations using AJAX.'),
+        '#markup' => $this->t('TODO: Display orders based on user permissions.'),
     );
   }
 }
