894ad679a0ce47ff1c648de7af3a70fe5a431088
Matthew Boyette <Dyndrilliac@gmail.com>
2016-11-30 09:21:22 -0500

    Update scripts

--- shell/drupal_init.sh
+++ shell/drupal_init.sh
@@ -48,10 +48,6 @@ sudo unzip highlight.zip
 sudo rm -f highlight.zip
 cd ../colorbox
 sudo wget https://github.com/jackmoore/colorbox/archive/1.x.zip
-sudo mv colorbox-1.x/* .
-sudo rm -rf colorbox-1.x
-sudo unzip colorbox-1.x.zip
-sudo rm -f colorbox-1.x.zip
 sudo unzip 1.x.zip
 sudo rm -f 1.x.zip
 sudo mv colorbox-1.x/* .
@@ -83,7 +79,27 @@ sudo wget https://ftp.drupal.org/files/projects/search_api-8.x-1.0-beta3.tar.gz
 sudo wget https://ftp.drupal.org/files/projects/token-8.x-1.0-beta2.tar.gz
 sudo wget https://ftp.drupal.org/files/projects/ubercart-8.x-4.0-alpha5.tar.gz
 sudo wget https://ftp.drupal.org/files/projects/vcl-8.x-1.1.tar.gz
-sudo tar -xvzf *.tar.gz
+sudo tar -xvzf addtoany-8.x-1.7.tar.gz
+sudo tar -xvzf admin_toolbar-8.x-1.17.tar.gz
+sudo tar -xvzf backup_migrate-8.x-4.0-alpha1.tar.gz
+sudo tar -xvzf bootstrap_layouts-8.x-3.1.tar.gz
+sudo tar -xvzf captcha-8.x-1.0-alpha1.tar.gz
+sudo tar -xvzf colorbox-8.x-1.2.tar.gz
+sudo tar -xvzf ctools-8.x-3.0-alpha27.tar.gz
+sudo tar -xvzf devel-8.x-1.0-alpha1.tar.gz
+sudo tar -xvzf ds-8.x-2.6.tar.gz
+sudo tar -xvzf entity-8.x-1.0-alpha3.tar.gz
+sudo tar -xvzf google_analytics-8.x-2.1.tar.gz
+sudo tar -xvzf layout_plugin-8.x-1.0-alpha23.tar.gz
+sudo tar -xvzf page_manager-8.x-1.0-alpha24.tar.gz
+sudo tar -xvzf panels-7.x-3.8.tar.gz
+sudo tar -xvzf pathauto-8.x-1.0-beta1.tar.gz
+sudo tar -xvzf php-8.x-1.0-beta2.tar.gz
+sudo tar -xvzf recaptcha-8.x-2.2.tar.gz
+sudo tar -xvzf search_api-8.x-1.0-beta3.tar.gz
+sudo tar -xvzf token-8.x-1.0-beta2.tar.gz
+sudo tar -xvzf ubercart-8.x-4.0-alpha5.tar.gz
+sudo tar -xvzf vcl-8.x-1.1.tar.gz
 sudo rm -f *.tar.gz
 cd ../custom
 sudo git clone https://github.com/Dyndrilliac/cen-4010-assignments.git
