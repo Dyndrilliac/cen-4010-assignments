928c8402dff6cbf9fcf291a2489316f86192c606
Matthew Boyette <Dyndrilliac@gmail.com>
2016-12-01 08:46:13 -0500

    Update module_update.sh

--- scripts/shell/module_update.sh
+++ scripts/shell/module_update.sh
@@ -6,15 +6,15 @@
 #############################################
 
 # Change directory to Drupal installation.
-cd /var/www/html
+cd /var/www/html/modules/custom/vmenu
 
 # Update local custom module repository from GitHub.
-cd modules/custom/vmenu
 sudo git stash
 sudo git checkout master
 sudo git fetch origin master
 sudo git rebase -i origin/master
 sudo git push origin master
+sudo git push drupal master
 sudo git stash pop
 
 # Copy theme data to themes directory.
