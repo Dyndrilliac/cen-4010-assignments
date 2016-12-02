ebebba627a6eaa73728db9d248d52c0958b8dce7
Matthew Boyette <Dyndrilliac@gmail.com>
2016-12-02 10:11:05 -0500

    Update theme

--- theme/vmenu/js/main.js
+++ theme/vmenu/js/main.js
@@ -1,4 +1,23 @@
 /**
+ * Simple-Slider
+ */
+
+$(function(){
+
+    // Initializing the swiper plugin for the slider.
+    // Read more here: http://idangero.us/swiper/api/
+    
+    var mySwiper = new Swiper ('.swiper-container', {
+        loop: true,
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev'
+    });
+    
+});
+
+/**
  * Place your custom scripts here.
  */
 
